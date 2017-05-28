d487ff437fb02bc4495830dfef56f9a62020501c
Justin Romanowski <just.roman7@gmail.com>
2017-05-28 11:39:26 -0700

    finished checkpoint 5

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,10 @@
+(function(){
+  function LandingCtrl() {
+    this.heroTitle = "turn the Music Up!";
+
+  }
+
+  angular
+    .module('blocJams')
+    .controller('LandingCtrl', LandingCtrl);
+})();
